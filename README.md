6eaa05f7d04fda98eff591b96a45f06784226865
Christian Madsen <christian.degnbol@gmail.com>
2021-12-02 13:22:10 +0100

    ProteinVolume 1.3

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 Prediction of reactivity for Glycosyl Transferase Superfamily 1 (GT1).
 
 ## REQUIREMENTS
-
 - unix shell with zsh
 - R
-- (mini)conda
+- pip
+- miniconda for python 3
+
+## OPTIONAL
+- ProteinVolume v1.3 downloaded from http://gmlab.bio.rpi.edu/download.php . A copy is found in `tools/ProteinVolume_1.3/`
 
 ## CONFIG
-- Configure some commands by running config.sh.
+- Configure some commands by running `./config.sh`.
 
 ## INSTALL
 - `./install.sh` will install python packages to env "gt". Activate with `conda activate gt`
--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env zsh
-# install python packages to conda environment "gt"
+# installing from requirements sets versions that was tested. There is some overlap in packages listed below.
 conda create --name gt --file requirements.txt
+
+# which packages are needed depends on what needs to be run.
+# we will need muscle for multiple sequence alignment and hmmer to do HMM alignment on multiple sequence alignments
+conda install -c bioconda muscle hmmer
+# RDKit is for physiochemcial properties of acceptor molecules. https://www.rdkit.org/docs/Install.html
+# It has to be a specific version so maybe have an isolated env for it
+conda install -c rdkit rdkit
+# 3D fingerprint
+conda install -c conda-forge e3fp
+# optionals for e3fp
+conda install -c conda-forge h5py standardiser
+
+pip install --upgrade cazy-parser
+# fix import bug so we get urllib.request loaded properly.
+sed -i '' 's/^import urllib$/import urllib.request/' ~/miniconda3/envs/gt/lib/python3*/site-packages/cazy_parser/create_cazy_db.py
+# I also commented out families that are not Glycosyltransferase since we don't care about those.
+
+# if we want to give attention to the balancing of positive and negative data points.
+conda install imbalanced-learn
+conda install cirpy # https://cirpy.readthedocs.io/en/latest/ conversion of CAS and other identifiers
+conda install multiprocess # fork of multiprocessing that uses dill instead of pickle which makes it possible to async call local functions
