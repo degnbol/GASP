4851a46de0e99cceec660b2c902c4cd59e9f1966
Christian Madsen <christian.degnbol@gmail.com>
2022-03-28 13:31:59 +1100

    root path

--- results/5-chemicalFeatures/acceptors-props/README.sh
+++ results/5-chemicalFeatures/acceptors-props/README.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # This folder contains chemistry features that were generated with the call
-../../src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
+`git root`/src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
