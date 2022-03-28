c1d8ed66c1a3dae01447134ce70c09d4369cd5e0
Christian Madsen <christian.degnbol@gmail.com>
2022-03-28 12:03:50 +1100

    generateResults.sh tested

--- results/6-features/rdkit-desc_muscle-ntern-hmm.sh
+++ results/6-features/rdkit-desc_muscle-ntern-hmm.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
-OUT=$ROOT/results/*features
-chemicals="$ROOT/results/*chemicalFeatures/acceptor_features.tsv"
-reactions="$ROOT/results/*generateNegatives/reactions.tsv"
+WORK=`ls -d $ROOT/results/*features`
+chemicals=`ls -d $ROOT/results/*chemicalFeatures/acceptor_features.tsv`
+reactions=`ls -d $ROOT/results/*generateNegatives/reactions.tsv`
 alignment="$ROOT/data/align/muscle_qual05.hmm.nterm.tsv"
 
 # create a feature set for classification where we discard uncertain data (reaction == 0.5)
 mlr --tsv --from "$chemicals" join -f "$reactions" -j cid then join -f "$alignment" -j enzyme then \
-    cut -x -f smiles then filter '$reaction != 0.5' > "$OUT/rdkit-desc_muscle-ntern-hmm.tsv"
+    cut -x -f smiles then filter '$reaction != 0.5' > "$WORK/rdkit-desc_muscle-ntern-hmm.tsv"
 
-mlr --tsv filter '$source == "GT-Predict"' rdkit-desc_muscle-ntern-hmm.tsv > "$OUT/rdkit-desc_muscle-ntern-hmm_gtpred.tsv"
-mlr --tsv filter '$source != "GT-Predict"' rdkit-desc_muscle-ntern-hmm.tsv > "$OUT/rdkit-desc_muscle-ntern-hmm_notgtpred.tsv"
+mlr --tsv filter '$source == "GT-Predict"' "$WORK/rdkit-desc_muscle-ntern-hmm.tsv" > "$WORK/rdkit-desc_muscle-ntern-hmm_gtpred.tsv"
+mlr --tsv filter '$source != "GT-Predict"' "$WORK/rdkit-desc_muscle-ntern-hmm.tsv" > "$WORK/rdkit-desc_muscle-ntern-hmm_notgtpred.tsv"
 
--- results/generateResults.sh
+++ results/generateResults.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # generate features for training. Unencoded, i.e. seqs are still in letter form, not numbers yet.
-`git root`/results/*/features/rdkit-desc_muscle-ntern-hmm.sh
+`git root`/results/*features/rdkit-desc_muscle-ntern-hmm.sh
