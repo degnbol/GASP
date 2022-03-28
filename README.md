6e3684db87e66dcdadf50fd493f3ea1f2decc4ec
Christian Madsen <christian.degnbol@gmail.com>
2022-03-28 11:47:44 +1100

    make comments clearer

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Prediction of reactivity for Glycosyl Transferase Superfamily 1 (GT1).
 - unix shell with zsh
 - R
 - pip
-- miniconda for python 3
+- miniconda for python 3, e.g. use `./install_conda.sh`
 
 ## OPTIONAL
 - open source pymol (pymol.org) to run chemical feature generation pipeline. E.g. install with `conda install -c schrodinger pymol-bundle`
--- install.sh
+++ install.sh
@@ -2,21 +2,11 @@
 # make sure you have miniconda installed
 conda --version &> /dev/null || ./install_conda.sh
 
-# installing from requirements sets versions that was tested. There is some overlap in packages listed below.
-conda create --name gt --file requirements.txt
+# Packages can be installing from requirements.txt with conda, then pip to set versions that were tested. 
+# Easier install without version requirements using packages that are all in conda:
+conda create --name gt --file requirements_conda.txt -c default -c conda-forge -c bioconda -c rdkit --yes
+conda activate gt
 
-# which packages are needed depends on what needs to be run.
-# we will need muscle for multiple sequence alignment and hmmer to do HMM alignment on multiple sequence alignments
-conda install -c bioconda muscle hmmer
-# RDKit is for physiochemcial properties of acceptor molecules. https://www.rdkit.org/docs/Install.html
-# It has to be a specific version so maybe have an isolated env for it
-conda install -c rdkit rdkit
-# 3D fingerprint
-conda install -c conda-forge e3fp
-# optionals for e3fp
-conda install -c conda-forge h5py standardiser
-
-# if we want to give attention to the balancing of positive and negative data points.
-conda install imbalanced-learn
-conda install cirpy # https://cirpy.readthedocs.io/en/latest/ conversion of CAS and other identifiers
-conda install multiprocess # fork of multiprocessing that uses dill instead of pickle which makes it possible to async call local functions
+# Optionally install interactive python and support for neovim editing
+conda install ipython
+conda install pynvim
