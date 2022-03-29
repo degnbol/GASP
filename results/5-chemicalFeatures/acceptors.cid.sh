92836d02d33866ce124ddaccdb3ce7ab590520e3
Christian Madsen <christian.degnbol@gmail.com>
2022-03-29 21:00:01 +1100

    simplify

--- results/5-chemicalFeatures/acceptors.cid.sh
+++ results/5-chemicalFeatures/acceptors.cid.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
-mlr --tsv uniq -f cid $ROOT/results/*-validateAcceptors/reactions.tsv \
-    $ROOT/data/acceptorsOfInterest/acceptors-of-interest.tsv | sed 1d |
-    cat - $ROOT/data/inhouse/david/negatives.cid |
-    grep -v '^$' | sort -u > acceptors.cid
+mlr --tsv uniq -f cid $ROOT/results/*-validateAcceptors/acceptors.tsv |
+    sed 1d | sort -u > acceptors.cid
