8543b3db13a23f6eb30c7db065d24a65cd496551
Christian Madsen <christian.degnbol@gmail.com>
2022-03-29 21:00:28 +1100

    add newly tested acceptors to pipeline

--- results/2-validateAcceptors/CO-.tsv.sh
+++ results/2-validateAcceptors/CO-.tsv.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 # Get the SMILES from CIDs for all chemicals that may be reactive or not.
 DATA=`git root`/data
-mlr --tsv uniq -f cid "$DATA/reactions/reactions.tsv" "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" | sed 1d |
-    cat - "$DATA/inhouse/david/negatives.cid" | sort -u |
+mlr --tsv uniq -f cid "$DATA/reactions/reactions.tsv" \
+    "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" \
+    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d |
+    cat - $DATA/inhouse/david/negatives.cid | sort -u |
     `git root`/src/chemistry/pubchem_props.R -p smiles |
     ./getCO-.py > CO-.tsv
 
--- results/2-validateAcceptors/acceptors.tsv.sh
+++ results/2-validateAcceptors/acceptors.tsv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 # Get the SMILES from CIDs for all chemicals that may be reactive or not.
 DATA=`git root`/data
-mlr --tsv uniq -f cid "reactions.tsv" "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" | sed 1d |
+mlr --tsv uniq -f cid "reactions.tsv" \
+    "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" \
+    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d |
     cat - "$DATA/inhouse/david/negatives.cid" | sort -u |
     `git root`/src/chemistry/pubchem_props.R -p smiles > acceptors.tsv
 
