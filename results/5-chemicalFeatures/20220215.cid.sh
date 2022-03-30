80a3727a3c44510b3f58879db4cce69782fc4c5f
Christian Madsen <christian.degnbol@gmail.com>
2022-03-30 20:56:04 +1100

    sed 1d

--- results/5-chemicalFeatures/20220215.cid.sh
+++ results/5-chemicalFeatures/20220215.cid.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-ROOT=`git root`
-mlr --tsv uniq -f cid $ROOT/data/inhouse/david/20220215__forML.xlsx.cas.tsv > 20220215.cid
+CID="`git root`/data/inhouse/david/20220215__forML.xlsx.cas.tsv"
+mlr --tsv uniq -f cid $CID | sed 1d > 20220215.cid
