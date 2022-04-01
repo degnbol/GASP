9ab3a43e5f6b08ad5b7096ad0af4cb21f6ea945d
Christian Madsen <christian.degnbol@gmail.com>
2022-04-01 15:47:13 +1100

    remove ignore flags so we don't get zero areas

--- src/chemistry/pymol_areas.sh
+++ src/chemistry/pymol_areas.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env zsh
 # Write pymol_areas.tsv given filenames of PDB files
 # REQUIREMENT: pymol installed and accessible with command `pymol`
-# USAGE: pymol_areas.sh PDBs/*.pdb > pymol_areas.tsv
-echo "cid\tSESA"
-for fname in $@; do
-	echo -n "${fname:t:r}\t"
-	pymol $fname -Q -cmd 'get_area' | sed 's/.*: //' | sed 's/ .*//'
+# USAGE: pymol_areas.sh PDBs/*.pdb pymol_areas.tsv > LOG
+INFILES=${@:1:-1}
+OUTFILE=$@[-1]
+
+# make sure OUTFILE is supplied.
+if [[ "${OUTFILE:e}" == "pdb" ]]; then
+	echo "OUTFILE not supplied" 1>&2
+	exit 1
+fi
+
+# after pymol 2.5 they started ignoring atoms with flag ignore,
+# which apparently was on all atoms
+CMD='flag ignore, all, clear; get_area' 
+
+echo "cid\tSESA" > $OUTFILE
+for fname in ${=INFILES}; do
+	name=${fname:t:r}
+	echo $name
+	echo -n "$name\t" >> $OUTFILE
+	pymol $fname -Q -cmd $CMD | sed 's/.*: //' | sed 's/ .*//' >> $OUTFILE
 done
