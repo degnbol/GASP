82e7dca9a492e850e5d17a30736d9bbb2be1db90
Christian Madsen <christian.degnbol@gmail.com>
2022-04-01 17:21:21 +1100

    use `ls ...*...`

--- results/6-features/traintest_cazy.sh
+++ results/6-features/traintest_cazy.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
 
-acceptorsOfInterest=$ROOT/results/*chemicalFeatures/acceptorsOfInterest.tsv
+acceptorsOfInterest=`ls $ROOT/results/*chemicalFeatures/acceptorsOfInterest.tsv`
 alignment="$ROOT/data/align/muscle_qual05.hmm.nterm.tsv"
+
+ids="enzyme acceptor source cid reaction rate"
 
 # for aaEnc in $ROOT/data/NCBI/{matchAmb,blosum62Amb}.tsv; do
 for aaEnc in $ROOT/data/NCBI/matchAmb.tsv; do
     cat rdkit-desc_muscle-ntern-hmm.tsv |
-        encode_features.py -i enzyme acceptor source cid reaction rate --aa seq --aa-encoding "$aaEnc" "$acceptorsOfInterest" "$alignment" |
+        encode_features.py -i ${=ids} --aa seq --aa-encoding "$aaEnc" \
+        "$acceptorsOfInterest" "$alignment" |
             gzip > traintest_${aaEnc:r:t}.tsv.gz
 done
 
