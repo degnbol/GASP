0c8b83658f0437e4fb8759f416657d54c86b4066
Christian Madsen <christian.degnbol@gmail.com>
2022-04-01 17:47:44 +1100

    more verbose. upd muscle syntax.

--- data/align/muscle.sh
+++ data/align/muscle.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env zsh
 # install muscle with conda install muscle -c bioconda
-muscle -quiet < ../sequences.faa | fasta.py unwrap > muscle.faa
+
+DATA=`git root`/data
+LIB=`git root`/tools/degnlib/subfunctions
+INFILE=$DATA/unaligned/sequences.faa
+CAZY=$DATA/CAZy/seqs_len.faa
+
+# align main sequences with muscle. takes time.
+muscle -align $INFILE -output muscle.faa.tmp
+$LIB/fasta_unwrap.py muscle.faa.tmp > muscle.faa && rm muscle.faa.tmp
+
+# write Hidden Markov Model from the alignment.
 hmmbuild --amino muscle.{hmm,faa} > muscle.hmm.log
-cat ../sequences.faa ../CAZy/seqs_len.faa | hmmalign --trim --amino --outformat A2M muscle.hmm - | fasta.py delete-lower > muscle.hmm.faa
-fasta.py filter-quality -t 0.8 muscle.hmm.faa muscle_qual05.hmm.faa
+
+# align all sequences to the HMM, then remove non-consensus.
+cat $INFILE $CAZY | hmmalign --trim --amino --outformat A2M muscle.hmm - |
+    $LIB/fasta_delete_lower.py > muscle.hmm.faa
+
+# remove low quality alignments
+$LIB/fasta_filter_quality.py -t 0.8 muscle.hmm.faa muscle_qual05.hmm.faa
