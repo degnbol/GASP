436ef75b626bbf62cd8bcdd0e754c3ef64f6c92c
Christian Madsen <christian.degnbol@gmail.com>
2022-04-01 15:48:55 +1100

    all features generated for old and new acceptors

--- results/5-chemicalFeatures/acceptors-props/README.sh
+++ results/5-chemicalFeatures/acceptors-props/README.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # This folder contains chemistry features that were generated with the call
-`git root`/src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
+/Users/degnbol/GT/src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
