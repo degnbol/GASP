dbbd45374b32fc9cc99981939e9c46a0c868d566
Christian Madsen <christian.degnbol@gmail.com>
2022-04-02 12:02:14 +1100

    hmm aligned seqs with newly tested seqs

--- data/align/muscle_nterm.sh
+++ data/align/muscle_nterm.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
+LIB=`git root`/tools/degnlib/subfunctions
 
 for file in muscle{,.hmm,_qual05.hmm}.faa; do
-    length=$(fasta.py length $file | sort -u)
+    length=$($LIB/fasta_length.py $file | sort -u)
     let length=length/2
-    fasta.py range $file -r 1-$length | fasta.py table -i enzyme -s seq > ${file:r}.nterm.tsv
+    $LIB/fasta_range.py $file -r 1-$length | $LIB/fasta_table.py -i enzyme -s seq > ${file:r}.nterm.tsv
 done
 
--- data/align/muscle_qual05.hmm.nterm.react.tsv.sh
+++ data/align/muscle_qual05.hmm.nterm.react.tsv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 # filter consensus alignment by enzymes that are found among the reactivity data.
-mlr --tsv --from ../reactions.tsv uniq -f enzyme then join -j enzyme -f muscle_qual05.hmm.nterm.tsv > muscle_qual05.hmm.nterm.react.tsv
+INFILE=`git root`/data/reactions/reactions.tsv
+mlr --tsv --from $INFILE uniq -f enzyme then join -j enzyme -f muscle_qual05.hmm.nterm.tsv > muscle_qual05.hmm.nterm.react.tsv
