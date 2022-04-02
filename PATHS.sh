de9467da6460272bcba0a502bea24ec1697fdca8
Christian Madsen <christian.degnbol@gmail.com>
2022-04-02 12:37:53 +1100

    more user-friendly comment

--- PATHS.sh
+++ PATHS.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
 echo "
-# Putting GT in your paths
+# Copy-paste these two lines to your ~/.zshrc to make sure all scripts can be found for GT
 export PATH=\"\$PATH:$ROOT/src:$ROOT/tools/degnlib\"
 export PYTHONPATH=\"\$PYTHONPATH:$ROOT:$ROOT/tools/degnlib\"
 "
