3b464d4ebe8864d569e8b2d89afca7ae15414a7e
Christian Madsen <christian.degnbol@gmail.com>
2022-04-02 14:46:13 +1100

    remove windows related chars

--- data/lit/reactions.tsv.sh
+++ data/lit/reactions.tsv.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env zsh
-mlr --icsv --otsv --from `git root`/data/lit/raw/sebastian/gt_all_data.csv join -j sugar_acceptor -f `git root`/data/lit/raw/sebastian/gt_acceptors_unique_CIDmanual.csv then \
+
+# fix microsoft related characters
+function unwin() {
+    tr "′’ʹ" "'" | tr '‐' '-' | tr -d ' '
+}
+
+unwin < raw/sebastian/gt_all_data.csv > tmp1.csv
+unwin < raw/sebastian/gt_acceptors_unique_CIDmanual.csv > tmp2.csv
+
+mlr --icsv --otsv --from tmp1.csv join -j sugar_acceptor -f tmp2.csv then \
     rename 'C-GT,enzyme,sugar_acceptor,acceptor,CID_acceptor,cid,diC-GT_activity,C_reaction,O-GT_activity,O_reaction,kobs_1/s,rate' then \
     put 'if ($C_reaction == "yes" || $O_reaction == "yes") {$reaction = 1} else {$reaction = 0}' then \
     cut -f enzyme,reaction,rate,cid,acceptor then put '$source = "sebastian"' then filter '$cid != ""' > reactions.tsv
+
+rm tmp?.csv
+
