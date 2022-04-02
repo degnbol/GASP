ea74ac96e959dc46630b706de0210ff00c3bd674
Christian Madsen <christian.degnbol@gmail.com>
2022-04-02 15:02:01 +1100

    use validated reactions.tsv

--- results/7-align/muscle_qual05.hmm.nterm.react.tsv.sh
+++ results/7-align/muscle_qual05.hmm.nterm.react.tsv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
 # filter consensus alignment by enzymes that are found among the reactivity data.
-INFILE=`git root`/data/reactions/reactions.tsv
+ROOT=`git root`
+INFILE=`ls $ROOT/results/*validateAcceptors/reactions.tsv`
 mlr --tsv --from $INFILE uniq -f enzyme then join -j enzyme -f muscle_qual05.hmm.nterm.tsv > muscle_qual05.hmm.nterm.react.tsv
