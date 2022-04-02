03d7b3b09af0ee8d39dd90b2f50b82fce220d927
Christian Madsen <christian.degnbol@gmail.com>
2022-04-02 14:51:12 +1100

    tsvlite now default

--- data/reactions/gtpred_reactions_acc.tsv.sh
+++ data/reactions/gtpred_reactions_acc.tsv.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env zsh
-cut -f2,23- raw/gtpred/acceptor_interaction_data.txt | sed '1s/Name/acceptor/' | sed '1s/UGT/At_/g' > gtpred_reactions_acc.tsv
+cut -f2,23- raw/gtpred/acceptor_interaction_data.txt |
+    sed '1s/Name/acceptor/' | sed '1s/UGT/At_/g' > gtpred_reactions_acc.tsv
--- data/reactions/rawAcceptor_cid_title.tsv.sh
+++ data/reactions/rawAcceptor_cid_title.tsv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 # you may need to first run `cd ../pubchem; ./CID-Title.sh; cd -`
-echo $'cid\ttitle' | cat - ../pubchem/CID-Title | mlr --tsvlite join -j cid -f rawAcceptor2cid.tsv > rawAcceptor_cid_title.tsv
+echo $'cid\ttitle' | cat - ../pubchem/CID-Title |
+    mlr --tsv join -j cid -f rawAcceptor2cid.tsv > rawAcceptor_cid_title.tsv
