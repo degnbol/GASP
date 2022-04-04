24f66ceec45d3ec49ee94e11b77722ff90042c11
Christian Madsen <christian.degnbol@gmail.com>
2022-04-04 11:44:47 +1000

    blosum seqs runs

---
+++ results/9-randomforest/expval/pred_seqs_blosum62Amb-speciesSeqSelect.tsv.gz.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+ROOT=`git root`
+INFILE=traintest_seqs_blosum62Amb.tsv.gz
+chemFeatures=$(gzcat $INFILE | head -n1 | tr '\t' '\n' | grep -v 'seq_' | tr '\n' ' ')
+features=$(cat $ROOT/results/*features/selection/species_select.txt | tr '\n' ' ')
+ids="enzyme acceptor source cid"
+gzcat $INFILE | randomforest.py -t 1 -i ${=ids} -c reaction -e rate -n 10000 -F ${=chemFeatures} ${=features} |
+    gzip > pred_seqs_blosum62Amb-speciesSeqSelect.tsv.gz
+
