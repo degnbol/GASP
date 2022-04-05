0f022446d69b2d91d7ad49c192dc9da151f31b06
Christian Madsen <christian.degnbol@gmail.com>
2022-04-05 13:43:30 +1000

    UGT==- -> ''

--- data/inhouse/david/ForChristian__Results_screening_of_47_polyphenols_against_40_GTs.xlsx.enzymes.tsv.sh
+++ data/inhouse/david/ForChristian__Results_screening_of_47_polyphenols_against_40_GTs.xlsx.enzymes.tsv.sh
@@ -2,3 +2,7 @@
 # enzyme table region from first sheet copy pasted then corrected and columns named.
 FILE='ForChristian__Results_screening_of_47_polyphenols_against_40_GTs.xlsx.enzymes.tsv'
 tr -d '\r' $FILE < | sed -E 's/^UGT ?//' | cat <(echo "ID\tDescription\tUGT\tAccession") - > temp && mv temp $FILE
+# UGT==- means no UGT entry
+mlr -I --tsv --from $FILE put 'if ($UGT == "-") {$UGT = ""}'
+# there is an entry missing for UGT number 9
+echo "9\t\t\tC0HFA0" >> $FILE
