851ca8a6307ae56e2910866d622d93e485445ce5
Christian Madsen <christian.degnbol@gmail.com>
2022-04-11 14:41:37 +1000

    aucs for requested thresholds

--- results/10-experimentalValidation/auc.tsv.sh
+++ results/10-experimentalValidation/auc.tsv.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
-for file in pred*-yield.tsv; do
-    echo -n "${file:r}\t"
-    mlr --tsv --from $file put '$class = $Yield < 50 ? 0 : 1' |
-        rocauc.py -H -p pred -c class /dev/stdin -o ${file:r}.png
-done > auc.tsv
+echo "file\tthres\tn0\tn1\tauc" > auc.tsv
+for thres in 25 50 75; do
+    for file in pred*-yield.tsv; do
+        echo -n "${file:r}\t$thres\t"
+        mlr --tsv --from $file put '$class = $Yield < '$thres' ? 0 : 1' > class.tsv.tmp
+        mlr --tsv --from class.tsv.tmp uniq -c -f class then cut -f count | sed 1d | tr '\n' '\t'
+        rocauc.py -H -p pred -c class -o /dev/null class.tsv.tmp
+    done
+done | tee -a auc.tsv
+
+rm class.tsv.tmp
 
 # we see that AUC is bad for chem files (novel acceptors)
 # but good on seqs files (novel sequences)
