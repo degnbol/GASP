506e1b66273d10f64636a72b25c92b365b9d7b89
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-05-26 16:18:46 +1000

    comment for clarity

--- results/10-experimentalValidation/expPred.sh
+++ results/10-experimentalValidation/expPred.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env zsh
+# Take prediction scores and join them with yield scores in experimentYield.tsv.
 ROOT=`git root`
 for file in $ROOT/results/*randomforest/expval/pred*.tsv.gz; do
     mlr --tsv --from $file join -j cid,enzyme -f experimentYield.tsv > ${file:t:r:r}-yield.tsv
