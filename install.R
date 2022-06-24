ccbfafa649b47ff9c6aac6bf565c79b30b499e14
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-06-24 19:24:22 +1000

    install packages for fatemeh eval

--- install.R
+++ install.R
@@ -3,3 +3,7 @@
 install.packages("data.table", type="source", repos="https://Rdatatable.gitlab.io/data.table")
 install.packages(c("webchem", "ggplot2", "Matrix", "tidyverse", "BiocManager", "optparse"), repos="https://cloud.r-project.org/")
 install.packages("rentrez")
+# R version of git root
+install.packages("here")
+# for data/Fatemeh_eval
+install.packages(c("reshape2", "pROC"))
