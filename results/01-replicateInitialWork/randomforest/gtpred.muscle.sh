63a7fe968f76ca90ab4248e25a67345effbaead3
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-06-27 18:10:50 +1000

    more relaxed setup for paths to look in

--- results/01-replicateInitialWork/randomforest/gtpred.muscle.sh
+++ results/01-replicateInitialWork/randomforest/gtpred.muscle.sh
@@ -1,1 +1,6 @@
-cat `git root`/GT-Predict/Active_enzymes_protein_sequences.txt | muscle -quiet 2> gtpred.muscle.log | fasta.py unwrap > gtpred.muscle.fa
+#!/usr/bin/env zsh
+export PYTHONPATH="$PYTHONPATH:`git root`/tools/degnlib"
+LIB=`git root`/tools/degnlib/subfunctions
+
+cat `git root`/data/GT-Predict/Active_enzymes_protein_sequences.txt | muscle -quiet 2> gtpred.muscle.log |
+    $LIB/fasta_unwrap.py > gtpred.muscle.fa
