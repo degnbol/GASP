27d4d07ccfc7f00dac5af89e63ec4375f37c3999
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-06-27 17:12:51 +1000

    decision tree fails for even a circular testing

--- results/01-replicateInitialWork/decision_tree.R
+++ results/01-replicateInitialWork/decision_tree.R
@@ -5,18 +5,22 @@ suppressPackageStartupMessages(library(here))
 # go to git root
 setwd(here())
 
-training_data = fread("data/acceptor_interaction_data.txt")
+training_data = fread("data/GT-Predict/acceptor_interaction_data.txt")
 classes_data = training_data[,-(1:22)]
 training_data = training_data[,3:22]
 training_data[is.na(training_data)] = ""
 
 classes = classes_data[[1]]
 valid = classes < 2 & !is.na(classes)
-# should actually be ntree=1 for a decision tree
+# ntree=1 for a decision tree
+tree = randomForest(training_data[valid,], factor(classes[valid]), ntree=1)
 forest = randomForest(training_data[valid,], factor(classes[valid]), ntree=100)
 
+# This is a circular test, trainset == testset, but it shows that a single tree 
+# is clearly worse, it can't even capture this artificially easy prediction well.
+cor(classes[valid], predict(tree, training_data[valid,]) == 1)
+cor(classes[valid], predict(tree, training_data[valid,], type="prob")[,"1"])
 cor(classes[valid], predict(forest, training_data[valid,]) == 1)
 cor(classes[valid], predict(forest, training_data[valid,], type="prob")[,"1"])
-
 
 
