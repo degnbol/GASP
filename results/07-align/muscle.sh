13f297dc1183b19ab06f4d970019722bbea3c0f0
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-06 21:35:54 +1000

    store intermediate with consensus indicated

--- results/07-align/muscle.sh
+++ results/07-align/muscle.sh
@@ -14,9 +14,10 @@ $LIB/fasta_unwrap.py muscle.faa.tmp > muscle.faa && rm muscle.faa.tmp
 # write Hidden Markov Model from the alignment.
 hmmbuild --amino muscle.{hmm,faa} > muscle.hmm.log
 
-# align all sequences to the HMM, then remove non-consensus.
-cat $INFILE $CAZY | hmmalign --trim --amino --outformat A2M muscle.hmm - |
-    $LIB/fasta_delete_lower.py > muscle.hmm.faa
+# align all sequences to the HMM
+cat $INFILE $CAZY | hmmalign --trim --amino --outformat A2M muscle.hmm - > muscle.hmm.a2m
+# remove non-consensus
+$LIB/fasta_delete_lower.py muscle.hmm.a2m > muscle.hmm.faa
 
 # remove low quality alignments
 $LIB/fasta_filter_quality.py -t 0.8 muscle.hmm.faa muscle_qual05.hmm.faa
