30282dbd14010a582a2f007d1d6724677280b81c
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-06 16:51:53 +1000

    don't assume path is set up

--- results/03-generateNegatives/reactions.tsv.sh
+++ results/03-generateNegatives/reactions.tsv.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env zsh
+LIB=`git root`/tools/degnlib/
+
 # Add negatives to the reaction data with the reaction bool = false.
 mlr --tsv --from ../*validateAcceptors/reactions.tsv uniq -f enzyme | sed 1d |
 while read enzyme; do
@@ -6,4 +8,4 @@ while read enzyme; do
 done |
     mlr --tsv filter '$cid != "cid"' then\
     put '$reaction = 0; $rate = ""; $source = "negatives"; $acceptor = ""' |
-    mlrcat.sh ../*validateAcceptors/reactions.tsv /dev/stdin > reactions.tsv
+    $LIB/mlr-cat ../*validateAcceptors/reactions.tsv /dev/stdin > reactions.tsv
