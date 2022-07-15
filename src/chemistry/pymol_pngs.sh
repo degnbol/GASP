8652d90c0db05890e73fa77d90968561bfed8fb4
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-15 20:33:33 +1000

    add option to hide hydrogens

--- src/chemistry/pymol_pngs.sh
+++ src/chemistry/pymol_pngs.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env zsh
 # Write .pngs with same filename as infiles in designated outdir.
 # REQUIREMENT: pymol installed and accessible with command `pymol`
-# USAGE: pymol_pngs.sh PDBs/ pngs/ [, width[, height[, dpi[, ray[, quiet]]]]]
+# USAGE: pymol_pngs.sh PDBs/ pngs/ [-H] [, width[, height[, dpi[, ray[, quiet]]]]]
 # https://pymolwiki.org/index.php/Png
 INDIR=$1
 OUTDIR=$2
-EXTRA=${@:3}
+if [[ "$3" == "-H" ]]; then
+    cmd0="hide (hydro); "
+    EXTRA=${@:4}
+else
+    cmd0=""
+    EXTRA=${@:3}
+fi
+
 
 # make sure dirs are supplied.
 if [[ "${OUTDIR:e}" == "pdb" ]]; then
@@ -18,5 +25,7 @@ mkdir -p $OUTDIR
 for fname in $INDIR/*.pdb; do
 	name=${fname:t:r}
 	echo $name
-	pymol $fname -Q -cmd "png $OUTDIR/$name.png $EXTRA"
+	
+	cmd="${cmd0}png $OUTDIR/$name.png $EXTRA"
+	pymol $fname -Q -cmd $cmd
 done
