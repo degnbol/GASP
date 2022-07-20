f9e50bd8cc82a7b440681cb375d49961e1ddf073
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-20 18:19:04 +1000

    also generate chem features for negatives

--- results/05-chemicalFeatures/acceptors.cid.sh
+++ results/05-chemicalFeatures/acceptors.cid.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
-mlr --tsv uniq -f cid $ROOT/results/*-validateAcceptors/acceptors.tsv |
+mlr --tsv uniq -f cid $ROOT/results/*-generateNegatives/reactions.tsv |
     sed 1d | sort -u > acceptors.cid
