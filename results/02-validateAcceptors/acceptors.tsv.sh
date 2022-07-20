30e630415be5309480549c452f4e03ee450fc742
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-20 18:14:02 +1000

    no longer using manually selected negatives

--- results/02-validateAcceptors/acceptors.tsv.sh
+++ results/02-validateAcceptors/acceptors.tsv.sh
@@ -3,7 +3,6 @@
 DATA=`git root`/data
 mlr --tsv uniq -f cid "reactions.tsv" \
     "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" \
-    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d |
-    cat - "$DATA/inhouse/david/negatives.cid" | sort -u |
+    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d | sort -u |
     `git root`/src/chemistry/pubchem_props.R -p smiles > acceptors.tsv
 
