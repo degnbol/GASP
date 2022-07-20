7a144cd7a8e5b094a0ae915a0c9cd4acd6fe46e6
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-20 21:54:38 +1000

    +LIB

--- results/09-randomforest/expval/pred.xlsx.sh
+++ results/09-randomforest/expval/pred.xlsx.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
+LIB=$ROOT/tools/degnlib/subfunctions
 
 for file in pred*.gz; do
     gzcat $file | mlr --tsv cut -f cid,enzyme,pred > $file:r
@@ -12,6 +13,6 @@ SHEETS="Overview species_selected_seq_features"
 SHEETS="${SHEETS} `for file in pred*.tsv; do echo $file:r | sed 's/pred_//' | sed 's/Amb//'; done`"
 echo $INFILES
 echo $SHEETS
-table.py excel ${=INFILES} -s ${=SHEETS} -o "pred.xlsx" -f
+$LIB/table_excel.py ${=INFILES} -s ${=SHEETS} -o "pred.xlsx" -f
 
 
