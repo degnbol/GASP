508a14ce1fde2d6c86f15e1be958987fdb8586bf
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-20 17:26:10 +1000

    gzcat -> zcat for linux compat

--- results/09-randomforest/expval/pred_seqs_blosum62Amb.tsv.gz.sh
+++ results/09-randomforest/expval/pred_seqs_blosum62Amb.tsv.gz.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 ids="enzyme acceptor source cid"
-gzcat traintest_seqs_blosum62Amb.tsv.gz |
+zcat traintest_seqs_blosum62Amb.tsv.gz |
     randomforest.py -t 1 -i ${=ids} -c reaction -e rate -n 10000 |
     gzip > pred_seqs_blosum62Amb.tsv.gz
 
