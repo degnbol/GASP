06ba776b7bdd9eb8dc2f446a37bff4ea66eb8411
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-25 18:25:52 +1000

    don't unpack, mlr can zcat

--- data/pubchem/CID-Title.sh
+++ data/pubchem/CID-Title.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env zsh
-path=`git root`/data/pubchem/CID-Title.gz
-wget -O $path ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Title.gz
-gunzip -k $path
+wget -O `git root`/data/pubchem/CID-Title.gz ftp://ftp.ncbi.nlm.nih.gov/pubchem/Compound/Extras/CID-Title.gz
