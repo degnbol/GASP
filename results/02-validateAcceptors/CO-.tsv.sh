d0b20e6ee9b4c77706761cca20ca7f71c0adb472
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-25 20:40:48 +1000

    negatives no longer manual

--- results/02-validateAcceptors/CO-.tsv.sh
+++ results/02-validateAcceptors/CO-.tsv.sh
@@ -3,8 +3,7 @@
 DATA=`git root`/data
 mlr --tsv uniq -f cid "$DATA/reactions/reactions.tsv" \
     "$DATA/acceptorsOfInterest/acceptors-of-interest.tsv" \
-    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d |
-    cat - $DATA/inhouse/david/negatives.cid | sort -u |
+    "$DATA/inhouse/david/20220215__forML.xlsx.cas.tsv" | sed 1d | sort -u |
     `git root`/src/chemistry/pubchem_props.R -p smiles |
     ./getCO-.py > CO-.tsv
 
