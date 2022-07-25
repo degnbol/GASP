4750e1eaaf1094bd545d7a794d87325ddd7953ce
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-25 11:18:22 +1000

    learnt about mlr sparsify,template,+

--- data/lit/UGT_reactivity_Natalia.AA.tsv.sh
+++ data/lit/UGT_reactivity_Natalia.AA.tsv.sh
@@ -4,5 +4,5 @@
 # some were looked up on uniprot and NCBI manually.
 # Do a left join where we annotate the manual ones and
 # emit unpaired from left file --ul to get the rest.
-mlr --tsv --from UGT_reactivity_Natalia.AA.manual.tsv cut -x -f Source then \
+mlr -t --from UGT_reactivity_Natalia.AA.manual.tsv cut -x -f Source then \
     join --ul -j Accession -f UGT_reactivity_Natalia.AA.tsv > temp && mv temp UGT_reactivity_Natalia.AA.tsv
--- data/lit/lit.tsv.sh
+++ data/lit/lit.tsv.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env zsh
-# current version of mlr needs | instead of 'then' due to what I have reported 
-# as a bug due to what I have reported as a bug due to what I have reported as 
-# a bug.
-mlr --tsv --from UGT_reactivity_Natalia.AA.tsv put '$Species = ""' |
-    mlr --tsv --from /dev/stdin --from 20201218_UGT_activitydata_from_lit.AA.tsv reorder -f `head -n1 UGT_reactivity_Natalia.AA.tsv |
-    tr '\t' ,` > lit.tsv
+mlr -t unsparsify UGT_reactivity_Natalia.AA.tsv 20201218_UGT_activitydata_from_lit.AA.tsv > lit.tsv
