ac19c5fa5ebb58e019e05643d9955ace8f2aad8f
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-25 20:37:46 +1000

    use uniq accessions instead of all rows

--- src/acc2aa.sh
+++ src/acc2aa.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
-# Writes a copy of INFILE.tsv to INFILE.aa.tsv with amino acid sequences curated from EMBL,
+# Writes a copy of INFILE.tsv to INFILE.AA.tsv with amino acid sequences curated from EMBL,
 # where INFILE.tsv must have EMBL accession ids in a column given as second arg, or by default "Accession".
 # USE: acc2aa.sh INFILE.tsv [Accession column name]
 INFILE=$1
@@ -12,13 +12,15 @@ function EMBL_get_trans() {
     grep '^FT' | sed 's/FT *//' | tr -d '\n' | sed 's/.*translation="//' | sed 's/".*//'
 }
 
-echo "AA" > $OUTFILE
+echo "$ACC_COL\tAA" > $OUTFILE
 
-mlr --tsv --from $INFILE cut -f $ACC_COL | sed 1d |
+mlr --tsv --from $INFILE uniq -f $ACC_COL | sed 1d |
 while read ACC; do
+    echo -n "$ACC\t" >> $OUTFILE
     curl https://www.ebi.ac.uk/ena/browser/api/embl/$ACC | EMBL_get_trans >> $OUTFILE
     echo "" >> $OUTFILE # newline
 done 
 
-TMP=$RANDOM.tmp
-paste $INFILE $OUTFILE > $TMP && mv $TMP $OUTFILE
+TMP=$RANDOM.tsv.tmp
+mlr --tsv --from $INFILE join -j $ACC_COL -f $OUTFILE > $TMP && mv $TMP $OUTFILE
+
