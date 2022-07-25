5bdf4adf3a07f7267b97bfe3de8c0dd72c9a7299
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-25 22:20:50 +1000

    use unsparsify to not worry about column order

--- results/03-generateNegatives/reactions.tsv.sh
+++ results/03-generateNegatives/reactions.tsv.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env zsh
-LIB=`git root`/tools/degnlib/
-
 # Add negatives to the reaction data with the reaction bool = false.
 mlr --tsv --from ../*validateAcceptors/reactions.tsv uniq -f enzyme | sed 1d |
 while read enzyme; do
@@ -8,5 +6,5 @@ mlr --tsv --from negatives.tsv cut -f cid then put '$enzyme = "'$enzyme'"'
 done | mlr --tsv filter '$cid != "cid"' then \
 put '$reaction = 0; $rate = ""; $source = "negatives"; $acceptor = ""' > reactions_neg.tsv
 
-$LIB/mlr-cat ../*validateAcceptors/reactions.tsv reactions_neg.tsv > reactions.tsv
+mlr -t unsparsify ../*validateAcceptors/reactions.tsv reactions_neg.tsv > reactions.tsv
 rm reactions_neg.tsv
