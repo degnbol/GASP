f345735d86117608d62190b222dd4008b837c95e
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-26 18:19:02 +1000

    second exp dataset features

---
+++ results/05-chemicalFeatures/20220215_features.tsv.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+`git root`/src/chemistry/pipeline.sh 20220215.cid 20220215_features.tsv
--- results/05-chemicalFeatures/acceptorsOfInterest.tsv.sh
+++ results/05-chemicalFeatures/acceptorsOfInterest.tsv.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env zsh
 # grab the collected acceptor features relevant to the CIDs for the select few acceptors that we will focus on.
-mlr --tsv --from acceptor_features.tsv join -j cid -f "`git root`/data/acceptorsOfInterest/acceptors-of-interest.tsv" > acceptorsOfInterest.tsv
+aoi=`git root`/data/acceptorsOfInterest/acceptors-of-interest.tsv
+mlr -t --from acceptor_features.tsv join -j cid -f $aoi > acceptorsOfInterest.tsv
