9a325407070f4ede2c6ec71a0b1b03fe784eb53d
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-26 11:48:36 +1000

    rerun of acceptor props with new lit + gtpred ext etc

--- results/05-chemicalFeatures/acceptors-props/README.sh
+++ results/05-chemicalFeatures/acceptors-props/README.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 # This folder contains chemistry features that were generated with the call
-/Users/degnbol/GT/src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
+/Users/cdmadsen/GT/src/chemistry/pipeline.sh acceptors.cid acceptor_features.tsv
