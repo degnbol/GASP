a96d5e07aa50c0364f27249e4f4b8c98bee7c6ff
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-07-26 12:09:03 +1000

    seqs added from lit + gtpred ext etc

--- results/06-unaligned/sequences.sh
+++ results/06-unaligned/sequences.sh
@@ -1,23 +1,33 @@
 #!/usr/bin/env zsh
 # Collect unaligned GT1 sequences.
+ROOT=`git root`
+DATA=$ROOT/data
+LIB=$ROOT/tools/degnlib/subfunctions
 
-DATA=`git root`/data
-LIB=`git root`/tools/degnlib/subfunctions
-
+# Initial GT-Predict, in-house
 sed '/>/!s/\*//' "$DATA/GT-Predict/Active_enzymes_protein_sequences.txt" \
     "$DATA/inhouse/hts_ugt/hts_ugt_constructs.fa" |
     sed '/^>/s/MT_71G1/Mt_71G1/' | sed 's/>UGT/>At_/' | $LIB/fasta_table.py |
     cat <(mlr --tsv --from "$DATA/inhouse/pTMH.tsv" uniq -f enzyme,sequence) - |
     mlr --tsv uniq -a > sequences.tsv
-mlr --tsv cut -f enzyme,sequence "$DATA/lit/sequences.tsv" | sed 1d >> sequences.tsv
+
+# GT-Predict ext
+mlr -t uniq -f Protein,AA "$DATA/GT-Predict/extensions.AA.tsv" | sed 1d >> sequences.tsv
+
+# lit
+mlr -t cut -f enzyme,sequence "$DATA/lit/sequences.tsv" | sed 1d >> sequences.tsv
+mlr -t uniq -f Protein,AA "$DATA/lit/lit.tsv" | sed 1d >> sequences.tsv
 
 # add new seqs that were tested experimentally
-mlr --tsv --from $DATA/inhouse/david/20220215__forML.xlsx.enzymes.AA.tsv cut -f ENA,AA | sed 1d >> sequences.tsv
+mlr -t --from $DATA/inhouse/david/20220215__forML.xlsx.enzymes.AA.tsv cut -f ENA,AA | sed 1d >> sequences.tsv
+
+mlr -t -I uniq -a sequences.tsv
 
 # the following code returns nothing when there are no duplicate entries, which is best.
+# Duplicates just means a bit of wasted effort in alignment, potentially.
 echo "Duplicate entries:"
-table_3D.R sequence < sequences.tsv | cut -f1 | grep ' '
+# use -D , so space isn't assumed to be a in-field sep
+table_3D.R sequence -D '|' < sequences.tsv | cut -f1 | grep '|'
 
-# also make a fasta copy
-$LIB/table_fasta.py -H sequences.tsv > sequences.faa
-
+# also make fasta version
+$LIB/table_fasta.py -d "\t" -H sequences.tsv > sequences.faa
