08a25dc6ff6f7c06f1fc81cae0a898ff17e07313
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-02 18:53:12 +1000

    DON+betanidin

---
+++ results/05-chemicalFeatures/DON+betanidin-props/README.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env zsh
+# This folder contains chemistry features that were generated with the call
+/Users/cdmadsen/GT/src/chemistry/pipeline.sh DON+betanidin.cid DON+betanidin_features.tsv acceptors2
