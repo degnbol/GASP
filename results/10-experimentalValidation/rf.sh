486349d36b94ee2267bd0a47a895758504422c62
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-02 18:54:03 +1000

    gzip and only test new seqs so far

--- results/10-experimentalValidation/rf.sh
+++ results/10-experimentalValidation/rf.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
 FEAT=`ls -d $ROOT/results/*-features/`
-chemFeat=`ls $ROOT/results/*-chemicalFeatures/*_features.tsv`
+chemFeat=`ls $ROOT/results/*-chemicalFeatures/{20220215,acceptors}_features.tsv`
 blosum62Amb=$FEAT/blosum62Amb.tsv.gz
 matchAmb=$FEAT/matchAmb.tsv.gz
 train=$FEAT/train.tsv
-testset=`ls $ROOT/results/*-experimentalValidation/experimentYield.tsv`
+# testset=`ls $ROOT/results/*-experimentalValidation/experimentYield.tsv`
+testset=seqTest.tsv
 
-# randomforest.py -i enzyme cid -c reaction -n 10000 -o matchAmb.rf.joblib -a ${=chemFeat} $matchAmb < $train
-# randomforest.py -i enzyme cid -c reaction -n 10000 -o blosum62Amb.rf.joblib -a ${=chemFeat} $blosum62Amb < $train
+randomforest.py -i enzyme cid -c reaction -n 10000 -o matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $train
+randomforest.py -i enzyme cid -c reaction -n 10000 -o blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $train
 
-randomforest.py -m matchAmb.rf.joblib -a ${=chemFeat} $matchAmb < $testset > matchAmb-pred.tsv
-randomforest.py -m blosum62Amb.rf.joblib -a ${=chemFeat} $blosum62Amb < $testset > blosum62Amb-pred.tsv
+randomforest.py -m matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $testset > matchAmb-pred.tsv
+randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $testset > blosum62Amb-pred.tsv
+
+auc -c Yield -t 50 -p pred blosum62Amb-pred.tsv -n
+auc -c Yield -t 50 -p pred matchAmb-pred.tsv -n
 
