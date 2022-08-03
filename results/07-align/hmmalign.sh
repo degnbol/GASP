4a95211f953fa061f46543a1efa93373c5feec0a
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-03 14:44:37 +1000

    remove unused train set filter file

--- results/07-align/hmmalign.sh
+++ results/07-align/hmmalign.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env zsh
-# Align sequences to hmm built from training data sequences in results/*-align/.
-# Low quality (>25% gap) alignments will be silently filtered out.
+# Align sequences to hmm built from training data sequences in results/*-align/,
+# silently filter out low quality (>25% gap) alignments, then take N-term half.
 # USE: results/*-align/hmmalign.sh < INFILE.faa
-# WRITES: muscle.hmm.a2m, muscle.hmm.faa, and muscle_qual.hmm.faa in pwd
+# WRITES: muscle.hmm.a2m, muscle.hmm.faa, and muscle_qual.hmm.faa, muscle_qual.hmm.nterm.tsv in pwd
 ROOT=`git root`
 LIB=$ROOT/tools/degnlib/subfunctions
 export PYTHONPATH="$PYTHONPATH:$ROOT/tools/degnlib"
@@ -13,4 +13,10 @@ hmmalign --trim --amino --outformat A2M $ROOT/results/*-align/muscle.hmm - > mus
 $LIB/fasta_delete_lower.py muscle.hmm.a2m > muscle.hmm.faa
 # remove low quality alignments
 $LIB/fasta_filter_quality.py -t 0.75 muscle.hmm.faa muscle_qual.hmm.faa
+
+# use only N-term (discard last half of alignments)
+length=$($LIB/fasta_length.py muscle_qual.hmm.faa | sort -u)
+let length=length/2
+$LIB/fasta_range.py muscle_qual.hmm.faa -r 1-$length |
+    $LIB/fasta_table.py -i enzyme -s seq > muscle_qual.hmm.nterm.tsv
 
--- results/07-align/muscle_qual.hmm.nterm.react.tsv.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-# filter consensus alignment by enzymes that are found among the reactivity data.
-ROOT=`git root`
-INFILE=`ls $ROOT/results/*validateAcceptors/reactions.tsv`
-mlr --tsv --from $INFILE uniq -f enzyme then \
-    join -j enzyme -f muscle_qual.hmm.nterm.tsv > muscle_qual.hmm.nterm.react.tsv
