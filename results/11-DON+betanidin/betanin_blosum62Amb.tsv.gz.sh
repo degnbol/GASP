64de656cb0958d85765642122f19751d202e729c
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-03 16:53:34 +1000

    keep all seqs to make sure trivial entries are present when asked for

--- results/11-DON+betanidin/betanin_blosum62Amb.tsv.gz.sh
+++ results/11-DON+betanidin/betanin_blosum62Amb.tsv.gz.sh
@@ -5,7 +5,8 @@ LIB=$ROOT/tools/degnlib/subfunctions
 mlr -t uniq -f enzyme,AA $ROOT/data/inhouse/david/betaninProd.tsv |
     $LIB/table_fasta.py -H | $ROOT/results/*-align/hmmalign.sh
 
+# -k to keep all features to make sure we have the ones present in the model.
 cat muscle_qual.hmm.nterm.tsv |
-    encode_features.py -i enzyme --aa seq --aa-encoding $ROOT/data/NCBI/blosum62Amb.tsv |
+    encode_features.py -k -i enzyme --aa seq --aa-encoding $ROOT/data/NCBI/blosum62Amb.tsv |
     gzip -c > betanin_blosum62Amb.tsv.gz
 
