cd554bb72ab7ea468ab3db424e06b081de67bf8f
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-03 16:53:11 +1000

    run betanin for cazy and specific seqs separately

--- results/11-DON+betanidin/rf.sh
+++ results/11-DON+betanidin/rf.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
-FEAT=`ls -d $ROOT/results/*-features/`
-chemFeat=`ls $ROOT/results/*-chemicalFeatures/acceptors2_features.tsv`
+chemFeat=`ls $ROOT/results/*-chemicalFeatures/{acceptors2,DON+betanidin}_features.tsv`
+# sequence features
+FEAT=`ls -d $ROOT/results/*-features`
 blosum62Amb=$FEAT/blosum62Amb.tsv.gz
 matchAmb=$FEAT/matchAmb.tsv.gz
-trainset=DON+betanidin-trainset.tsv
-testset=DON+betanidin-testset.tsv
 
-randomforest.py -i enzyme cid -c reaction -n 10000 -o blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $train
-randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $testset > blosum62Amb-pred.tsv
-auc -c Yield -t 50 -p pred blosum62Amb-pred.tsv -n
+# train
+# randomforest.py -i enzyme cid -c reaction -n 10000 -o blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < DON+betanidin-trainset.tsv
+
+# test
+randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < DON-testset.tsv > DON-pred.tsv
+randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < betanin-testset.tsv |
+    mlr -t filter '$pred != ""' > betaninCazy-pred.tsv
+randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} betanin_blosum62Amb.tsv.gz < betanin-testset.tsv |
+    mlr -t filter '$pred != ""' > betanin-pred.tsv
+
 
