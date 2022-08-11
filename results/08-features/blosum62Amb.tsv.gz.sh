cc83a778e0ab9a477cd12ff0437723f07c5bb497
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-11 19:15:48 +1000

    result ordering agnostic

--- results/08-features/blosum62Amb.tsv.gz.sh
+++ results/08-features/blosum62Amb.tsv.gz.sh
@@ -2,6 +2,6 @@
 # simply encode all sequences with blosum62 (with ambiguous codes).
 # All features were kept, but one could use -k flag to ensure.
 ROOT=`git root`
-cat $ROOT/results/07-align/muscle_qual.hmm.nterm.tsv |
+cat $ROOT/results/*-align/muscle_qual.hmm.nterm.tsv |
     encode_features.py -i enzyme --aa seq --aa-encoding $ROOT/data/NCBI/blosum62Amb.tsv |
     gzip -c > blosum62Amb.tsv.gz
