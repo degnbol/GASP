dc8015c9d9e3505755dbd62d3d8586a99ffbab5f
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-15 15:07:26 +1000

    auc and acc for all dataset 2 preds

---
+++ results/10-experimentalValidation/pred_all-aucChem.tsv.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+auc pred_all.tsv -c Yield -t 50 -g 'Polyphenol Name' -p pred -n -b -a -C > pred_all-aucChem.tsv
---
+++ results/10-experimentalValidation/pred_all-aucEnz.tsv.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env zsh
+auc pred_all.tsv -c Yield -t 50 -g enzyme -p pred -n -b -a -C > pred_all-aucEnz.tsv
