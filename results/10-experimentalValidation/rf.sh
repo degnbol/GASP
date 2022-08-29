56f6cbada065b930621a975f940ee169c4f0d740
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-08-29 18:30:43 +1000

    various runs

--- results/10-experimentalValidation/rf.sh
+++ results/10-experimentalValidation/rf.sh
@@ -6,17 +6,49 @@ chemFeat=`ls $ROOT/results/*-chemicalFeatures/acceptors2_features.tsv`
 blosum62Amb=$FEAT/blosum62Amb.tsv.gz
 matchAmb=$FEAT/matchAmb.tsv.gz
 train=$FEAT/train.tsv
-testset=seqTest.tsv
-testsetAll=`ls $ROOT/results/*-experimentalValidation/experimentYield.tsv`
+trainSmpl=$FEAT/train_negativesSample.tsv
+testsetAll=experimentYield.tsv
+testsetDist=testsetDist.tsv
+testsetSeq=testsetNovelSeqs.tsv
+testsetChem=testsetNovelChems.tsv
 
-randomforest.py -i enzyme cid -c reaction -n 10000 -o matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $train
-randomforest.py -i enzyme cid -c reaction -n 10000 -o blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $train
+# randomforest.py -i enzyme cid -c reaction -n 1000 -o matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $train
+# randomforest.py -i enzyme cid -c reaction -n 1000 -o blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $train
+# randomforest.py -i enzyme cid -c reaction -n 1000 --class-weight balanced -o balance_matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $train
+# randomforest.py -i enzyme cid -c reaction -n 1000 --class-weight balanced -o balance_blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $train
+# randomforest.py -i enzyme cid -c reaction -n 1000 -o smpl_matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $trainSmpl
+# randomforest.py -i enzyme cid -c reaction -n 1000 -o smpl_blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $trainSmpl
+# randomforest.py -i enzyme cid -c reaction -n 1000 --class-weight balanced -o smpl_balance_matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $trainSmpl
+# randomforest.py -i enzyme cid -c reaction -n 1000 --class-weight balanced -o smpl_balance_blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $trainSmpl
 
-randomforest.py -m matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $testset > matchAmb-pred.tsv
-randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $testset > blosum62Amb-pred.tsv
-randomforest.py -m blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $testsetAll > pred_all.tsv
+# echo match
+# for model in *matchAmb.rf.joblib.gz; do
+#     echo $model
+#     echo all
+#     randomforest.py -m $model -a ${=chemFeat} $matchAmb < $testsetAll  | auc -c Yield -t 50 -p pred -n -a
+#     echo dist
+#     randomforest.py -m $model -a ${=chemFeat} $matchAmb < $testsetDist | auc -c Yield -t 50 -p pred -n -a
+#     echo seq
+#     randomforest.py -m $model -a ${=chemFeat} $matchAmb < $testsetSeq  | auc -c Yield -t 50 -p pred -n -a
+#     echo chem
+#     randomforest.py -m $model -a ${=chemFeat} $matchAmb < $testsetChem | auc -c Yield -t 50 -p pred -n -a
+# done
+#
+# echo blosum
+# for model in *blosum62Amb.rf.joblib.gz; do
+#     echo $model
+#     echo all
+#     randomforest.py -m $model -a ${=chemFeat} $blosum62Amb < $testsetAll  | auc -c Yield -t 50 -p pred -n -a
+#     echo dist
+#     randomforest.py -m $model -a ${=chemFeat} $blosum62Amb < $testsetDist | auc -c Yield -t 50 -p pred -n -a
+#     echo seq
+#     randomforest.py -m $model -a ${=chemFeat} $blosum62Amb < $testsetSeq  | auc -c Yield -t 50 -p pred -n -a
+#     echo chem
+#     randomforest.py -m $model -a ${=chemFeat} $blosum62Amb < $testsetChem | auc -c Yield -t 50 -p pred -n -a
+# done
 
-auc -c Yield -t 50 -p pred matchAmb-pred.tsv -n
-auc -c Yield -t 50 -p pred blosum62Amb-pred.tsv -n
-auc -c Yield -t 50 -p pred pred_all.tsv -n
+# randomforest.py -m smpl_matchAmb.rf.joblib.gz -a ${=chemFeat} $matchAmb < $testsetAll > smpl_matchAmb-pred.tsv
+# auc smpl_matchAmb-pred.tsv -c Yield -t 50 -p pred -n -a
 
+randomforest.py -m smpl_blosum62Amb.rf.joblib.gz -a ${=chemFeat} $blosum62Amb < $testsetAll > smpl_blosum62Amb-pred.tsv
+auc smpl_blosum62Amb-pred.tsv -c Yield -t 50 -p pred -n -a
