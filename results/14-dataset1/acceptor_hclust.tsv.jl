b6ec9044fca858ef9e6c49a4ec02799e8017519b
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2022-12-21 18:54:06 +1100

    acceptor hclust

---
+++ results/14-dataset1/acceptor_hclust.tsv.jl
@@ -0,0 +1,23 @@
+#!/usr/bin/env julia
+# hierarchical clustering of chemicals for visuals.
+using Clustering
+using DataFrames, CSV
+using Statistics
+using Distances
+
+df = CSV.read("dataset1.tsv", DataFrame)
+
+df_mat = unstack(df, :acceptor, :enzyme, :rate)
+mat = Matrix(df_mat[!, 2:end])
+
+# replace missing values with average rate per enzyme
+avg_enz_rates = mat |> eachcol .|> skipmissing .|> mean
+for (col, avg_rate) in enumerate(avg_enz_rates)
+    mat[ismissing.(mat[:, col]), col] .= avg_rate
+end
+
+dists = pairwise(Euclidean(), mat; dims=1)
+
+ordering = hclust(dists).order
+out = DataFrame(acceptor=df_mat.acceptor, order=ordering)
+CSV.write("acceptor_hclust.tsv", out; delim='\t')
