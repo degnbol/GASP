8678acec1568a47479f398fee0d83cd7a42c1eca
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2023-03-02 13:12:05 +1100

    autoenv files

---
+++ .activate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# this file can be used with a tool such as https://github.com/zpm-zsh/autoenv
+conda activate GT
+export ROOT=$0:h
---
+++ .deactivate.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# this file can be used with a tool such as https://github.com/zpm-zsh/autoenv
+conda deactivate
+export ROOT=
