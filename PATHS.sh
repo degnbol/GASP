ee090e37034307fce7de121d0bc4cfbe831b6e85
Christian Degnbol Madsen <christian.degnbol@gmail.com>
2024-11-20 16:32:58 +1100

    Minor message change: GT->GASP

--- PATHS.sh
+++ PATHS.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 ROOT=`git root`
 echo "
-# Copy-paste these two lines to your ~/.zshrc to make sure all scripts can be found for GT
+# Copy-paste these two lines to your ~/.zshrc to make sure all scripts can be found for GASP
 export PATH=\"\$PATH:$ROOT/src:$ROOT/tools/degnlib\"
 export PYTHONPATH=\"\$PYTHONPATH:$ROOT:$ROOT/tools/degnlib\"
 "
